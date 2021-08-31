5c4043eb08c5b07018c2ace15b8ac1ec4738d080
ulaecio <ulaecio@live.com>
2021-08-30 23:56:36 -0300

    Project created

---
+++ backend/src/main/java/com/ulasoftware/uspesquisa/UspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.uspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UspesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ulasoftware/uspesquisa/UspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.uspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
